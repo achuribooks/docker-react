4ccf299fc5f0e442206e4710de6d37c880bc321c
achuribooks <achuribooks@gmail.com>
2024-11-18 19:58:19 -0500

    minor text change to app.js to see results automatically deployed live

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there betch! MWAHAHAHA. STILL WORKSSSSS
+          I HAVE MADE PROGRESSO SOUP!
         </p>
         <a
           className="App-link"
